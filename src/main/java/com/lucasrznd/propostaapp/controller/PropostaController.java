a234745dfd9dc56a7f3a956c5de5d498b2eab39d
Lucas Rezende <lucasffrezende@gmail.com>
2024-06-19 18:06:14 -0300

    ✨ Criado PropostaService

--- src/main/java/com/lucasrznd/propostaapp/controller/PropostaController.java
+++ src/main/java/com/lucasrznd/propostaapp/controller/PropostaController.java
@@ -2,19 +2,25 @@ package com.lucasrznd.propostaapp.controller;
 
 import com.lucasrznd.propostaapp.dto.PropostaRequestDTO;
 import com.lucasrznd.propostaapp.dto.PropostaResponseDTO;
+import com.lucasrznd.propostaapp.service.PropostaService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/proposta")
 public class PropostaController {
 
+    private PropostaService propostaService;
+
     @PostMapping
     public ResponseEntity<PropostaResponseDTO> create(@RequestBody PropostaRequestDTO requestDTO) {
-        return null;
+        PropostaResponseDTO response = propostaService.create(requestDTO);
+        return ResponseEntity.ok(response);
     }
 
 }
---
+++ src/main/java/com/lucasrznd/propostaapp/service/PropostaService.java
@@ -0,0 +1,14 @@
+package com.lucasrznd.propostaapp.service;
+
+import com.lucasrznd.propostaapp.dto.PropostaRequestDTO;
+import com.lucasrznd.propostaapp.dto.PropostaResponseDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PropostaService {
+
+    public PropostaResponseDTO create(PropostaRequestDTO requestDTO) {
+        return null;
+    }
+
+}
