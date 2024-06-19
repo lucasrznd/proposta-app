fb3706cdb264fb8051fac0481a5eb1f80da78f63
Lucas Rezende <lucasffrezende@gmail.com>
2024-06-19 17:20:44 -0300

    ✨ Criando entidades

---
+++ src/main/java/com/lucasrznd/propostaapp/entities/Proposta.java
@@ -0,0 +1,29 @@
+package com.lucasrznd.propostaapp.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_proposta")
+public class Proposta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double valorSolicitado;
+    private int prazoPagamento;
+    private Boolean aprovada;
+    private boolean integrada;
+    private String observacao;
+
+    @OneToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+}
---
+++ src/main/java/com/lucasrznd/propostaapp/entities/Usuario.java
@@ -0,0 +1,27 @@
+package com.lucasrznd.propostaapp.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String sobrenome;
+    private String cpf;
+    private String telefone;
+    private Double renda;
+
+    @OneToOne(mappedBy = "usuario")
+    private Proposta proposta;
+
+}
