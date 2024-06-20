87141ec0ca638f3767c388faca3dffbd35553973
Lucas Rezende <lucasffrezende@gmail.com>
2024-06-20 07:54:55 -0300

    ✨ Adicionado findAll

--- src/main/java/com/lucasrznd/propostaapp/controller/PropostaController.java
+++ src/main/java/com/lucasrznd/propostaapp/controller/PropostaController.java
@@ -5,11 +5,14 @@ import com.lucasrznd.propostaapp.dto.PropostaResponseDTO;
 import com.lucasrznd.propostaapp.service.PropostaService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -17,6 +20,11 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 public class PropostaController {
 
     private PropostaService propostaService;
+
+    @GetMapping
+    public ResponseEntity<List<PropostaResponseDTO>> findAll() {
+        return ResponseEntity.ok(propostaService.findAll());
+    }
 
     @PostMapping
     public ResponseEntity<PropostaResponseDTO> create(@RequestBody PropostaRequestDTO requestDTO) {
--- src/main/java/com/lucasrznd/propostaapp/service/PropostaService.java
+++ src/main/java/com/lucasrznd/propostaapp/service/PropostaService.java
@@ -8,11 +8,17 @@ import com.lucasrznd.propostaapp.repository.PropostaRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @AllArgsConstructor
 @Service
 public class PropostaService {
 
     private PropostaRepository propostaRepository;
+
+    public List<PropostaResponseDTO> findAll() {
+        return PropostaMapper.INSTANCE.toListDTO(propostaRepository.findAll());
+    }
 
     public PropostaResponseDTO create(PropostaRequestDTO requestDTO) {
         Proposta proposta = PropostaMapper.INSTANCE.toEntity(requestDTO);
