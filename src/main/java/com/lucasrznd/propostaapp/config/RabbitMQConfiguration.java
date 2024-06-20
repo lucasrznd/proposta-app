6e43468e2ecc4f272acef701b827f2f5cc197aee
Lucas Rezende <lucasffrezende@gmail.com>
2024-06-20 17:00:23 -0300

    ✨ Criado RabbitAdmin

--- src/main/java/com/lucasrznd/propostaapp/config/RabbitMQConfiguration.java
+++ src/main/java/com/lucasrznd/propostaapp/config/RabbitMQConfiguration.java
@@ -2,6 +2,10 @@ package com.lucasrznd.propostaapp.config;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -26,6 +30,16 @@ public class RabbitMQConfiguration {
     @Bean
     public Queue criarFilaPropostaConcluidaMsNotificacao() {
         return QueueBuilder.durable("proposta-concluida.ms-notificacao").build();
+    }
+
+    @Bean
+    public RabbitAdmin createRabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> initAdmin(RabbitAdmin rabbitAdmin) {
+        return event -> rabbitAdmin.initialize();
     }
 
 }
